46fae6a1f70c39a26e666414787e872e864af17a
luisFebro <mr.febro@gmail.com>
2024-01-08 06:13:58 -0300

    updating: conclusion

--- app/src/main/java/com/febro/tfsouthamericalandmarkdetector/MainActivity.kt
+++ app/src/main/java/com/febro/tfsouthamericalandmarkdetector/MainActivity.kt
@@ -1,43 +1,98 @@
 package com.febro.tfsouthamericalandmarkdetector
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.febro.tfsouthamericalandmarkdetector.data.TfLiteLandmarkClassifier
+import com.febro.tfsouthamericalandmarkdetector.domain.Classification
+import com.febro.tfsouthamericalandmarkdetector.presentation.CameraPreview
+import com.febro.tfsouthamericalandmarkdetector.presentation.LandmarkImageAnalyzer
 import com.febro.tfsouthamericalandmarkdetector.ui.theme.TfSouthAmericaLandmarkDetectorTheme
 
+
+// ref: https://www.youtube.com/watch?v=ViRfnLAR_Uc
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if(!hasCameraPermission()) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 0)
+        }
+
         setContent {
             TfSouthAmericaLandmarkDetectorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                var classifications by remember {
+                    mutableStateOf(emptyList<Classification>())
+                }
+
+                val analyser = remember {
+                    LandmarkImageAnalyzer(
+                        classifier = TfLiteLandmarkClassifier(
+                            context = applicationContext
+                        ),
+                        onResults = {
+                            classifications = it
+                        }
+                    )
+                }
+
+                val controller = remember {
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+                        setImageAnalysisAnalyzer(
+                            ContextCompat.getMainExecutor(applicationContext),
+                            analyser
+                        )
+                    }
+                }
+
+                Box(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    CameraPreview(controller, Modifier.fillMaxSize())
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.TopCenter)
+                    ) {
+                        classifications.forEach {
+                            Text(
+                                text = it.name,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(MaterialTheme.colorScheme.primaryContainer)
+                                    .padding(8.dp),
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TfSouthAmericaLandmarkDetectorTheme {
-        Greeting("Android")
-    }
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(
+        this,
+        android.Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
 }
